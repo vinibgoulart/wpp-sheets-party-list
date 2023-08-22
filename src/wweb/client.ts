f035600ac32389029b1c1abdfbfeec73c8f85dad
Vinicius <viblaziusgoulart@gmail.com>
2023-08-22 07:57:39 -0300

    feat(qrcode): use qrcode-terminal lib

--- src/wweb/client.ts
+++ src/wweb/client.ts
@@ -2,16 +2,17 @@ import { Client, LocalAuth } from "whatsapp-web.js";
 import { onReady } from "./ready";
 import { onMessage } from "./onMessage";
 import { onAuthFailure } from "./onAuthFailure";
+import { onQrCode } from "./onQrCode";
 
 export const client = new Client({
   authStrategy: new LocalAuth(),
-  puppeteer: {
-    headless: false,
-  },
 });
 
 export const connectClient = () => {
+  console.log("initializing client");
   client.initialize();
+
+  onQrCode();
 
   onReady();
 
